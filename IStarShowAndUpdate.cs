79c5cb4c284e2659d281f1d3453cd88b0969eda1
NecromancerMidas <115973453+NecromancerMidas@users.noreply.github.com>
2023-04-19 13:59:54 +0200

    Add project files.

---
+++ IStarShowAndUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStars
+{
+    internal interface IStarShowAndUpdate
+    {
+        void Show();
+        void Update();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+namespace ConsoleStars
+{
+    class Program
+    {      
+        static void Main(string[] args)
+        {
+            var random = new Random(Guid.NewGuid().GetHashCode());
+            
+
+
+            var stars = new List<IStarShowAndUpdate>()
+            {
+          /*      new PhasesStar(random),
+                new PhasesStar(random),
+                new PhasesStar(random),*/
+           /*     new MovableStar(random),
+                new MovableStar(random),
+                new MovableStar(random),*/
+            };
+            for (int i = 3; i > 0; i--)
+            {
+                stars.Add(new PhasesStar(random));
+                stars.Add(new MovableStar(random));
+            }
+            
+            ;
+            while (true)
+            {
+                Console.Clear();     
+                foreach (var star in stars)
+                {
+                    star.Show();
+                    star.Update();
+                }
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
